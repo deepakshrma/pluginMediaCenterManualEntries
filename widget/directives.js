d9cd568f7700d3651643065917f93e018f330bcf
lakshaydulani <lakshaydulani@gmail.com>
2016-04-13 13:59:20 +0530

    local image implemented

--- widget/directives.js
+++ widget/directives.js
@@ -26,19 +26,31 @@
                     element.attr("src", "../../../styles/media/holder-" + attrs.loadImage + ".gif");
 
 
-                    var elem = $("<img>");
-                    elem[0].onload = function () {
-                        element.attr("src", attrs.finalSrc);
-                        elem.remove();
-                    };
+                  /*   var elem = $("<img>");
+                     elem[0].onload = function () {
+                     debugger;
+                     element.attr("src", attrs.finalSrc);
+                     elem.remove();
+                     };*/
 
-                    function changeSrc(info) {
-                        element.attr("src", attrs.finalSrc);
-                        elem.remove();
+                    function changeSrc() {
+                        var size = attrs.cropSize.split('X'),
+                            width = size[0],
+                            height = size[1];
+                        buildfire.imageLib.local.cropImage(attrs.finalSrc, {
+                            width: width,
+                            height: height
+                        }, function (e, d) {
+                            if (!e) {
+                                element.attr("src", d);
+                            }
+                        });
+                        //elem.remove();
                     }
-                   scope.$watch(function(val){
-                       return attrs.finalSrc;
-                   }, changeSrc, true);
+
+                    scope.$watch(function (val) {
+                        return attrs.finalSrc;
+                    }, changeSrc, true);
                     elem.attr("src", attrs.finalSrc);
                 }
             };
